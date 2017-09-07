d55012707735be44f0f0df6cbc2c3e2b3cd01941
johndoe <nicoaudy1994@gmail.com>
2017-09-07 14:30:14 +0700

    showing timeline based on api with vue

---
+++ resources/assets/js/components/Timeline.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-info">
+                    <div class="panel-heading">Timeline</div>
+
+                    <div class="panel-body">
+                        <div class="media" v-for="tweet in tweets">
+                            <div class="media-left">
+                                <a href="">
+                                    <img src="http://lorempixel.com/60/60" alt="tweet.user.name">
+                                </a>
+                            </div>
+                            <div class="media-body">
+                                <strong>{{ tweet.user.name }}</strong>
+                                <p>{{ tweet.body }}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                tweets: []
+            }
+        },
+        mounted() {
+            console.log('Component mounted.')
+            axios.get('tweets').then(response => {
+                console.log(response)
+                this.tweets = response.data
+            })
+        }
+    }
+</script>
